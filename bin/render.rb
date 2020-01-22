249d5fc681390cc03987dd88511061c7a94172df
onecaym <osipov99@list.ru>
2020-01-22 11:28:24 +0400

    added progressbar and done

--- bin/render.rb
+++ bin/render.rb
@@ -2,17 +2,16 @@
 require_relative '../lib/movie_collection.rb'
 require_relative '../lib/renderer.rb'
 require_relative '../lib/downloader.rb'
+require 'progressbar'
 require 'haml'
 require 'yaml'
 
 yaml_lib = File.open('../lib/data/libs.yaml')
 collection = Kino::MovieCollection.new('../lib/data/movies.txt')
-puts yaml_lib.any?
+yaml_lib.any?
 unless yaml_lib.any?
   loader = Kino::Downloader.new(collection)
   loader.start
 end
-loader = Kino::Downloader.new(collection)
-  loader.start
 rend = Kino::Renderer.new(collection)
 rend.start
--- lib/downloader.rb
+++ lib/downloader.rb
@@ -4,7 +4,7 @@ require 'themoviedb'
 require 'dotenv/load'
 require 'yaml'
 require_relative '../lib/tmdb.rb'
-require 'progress_bar'
+require 'ruby-progressbar'
 
 module Kino
   class Downloader
@@ -15,8 +15,9 @@ module Kino
 
     def start
       extend MovieDB
+      bar = ProgressBar.create(total: 250, :format => '%a %B %p%% %t')
       Tmdb::Api.key(ENV['TMDBKEY'])
-      saved_info = @movies.map { |movie| p [movie.movie_id, { picture: movie.picturelink(movie.movie_id), translate: movie.translation(movie.movie_id), budget: movie.filmbudget(movie.link) }] }.to_h # rubocop:disable Metrics/LineLength
+      saved_info = @movies.map { |movie| bar.increment; [ movie.movie_id, { picture: movie.picturelink(movie.movie_id), translate: movie.translation(movie.movie_id), budget: movie.filmbudget(movie.link) }] }.to_h # rubocop:disable Metrics/LineLength
       File.write(@direction, saved_info.to_yaml)
     end
   end
